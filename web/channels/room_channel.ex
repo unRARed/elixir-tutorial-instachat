c719050dcc6a8df865ffa4efd1cd741960f435e4
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-01-02 19:34:43 -0800

    Hook up basic input broadcast

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -5,4 +5,11 @@ defmodule Instachat.RoomChannel do
   def join(room, params, socket) do
     {:error, %{reason: "you can only join the lobby"}}
   end
+
+  # broadcasts incoming message from
+  # jQuery $chatInput element to all clients
+  def handle_in("new_message", body, socket) do
+    broadcast! socket, "new_message", body
+    {:noreply, socket}
+  end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,9 +54,24 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("rooms:lobby", {})
+let channel = socket.channel("rooms:lobby", {});
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) });
 
 export default socket
+
+let $chatInput = $('#chat-input');
+let $messagesContainer = $('#messages');
+
+$chatInput.on('keypress', event => {
+  if (event.keyCode === 13) {
+    // to be processed by Instachat.RoomChannel handle_in
+    channel.push('new_message', {body:$chatInput.val()});
+    $chatInput.val('');
+  }
+});
+
+channel.on('new_message', payload => {
+  $messagesContainer.append(`<br>[${Date()}] ${payload.body}`);
+});
